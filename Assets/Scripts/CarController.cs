a46e69b26fa1677857d5dad8d7d8c28976802066
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-02-25 11:10:45 +0100

    Scene and network
    A new scene is added as test environment.
    This scene has some files added for multiplayer (SceneLevelMultiplayer).
    
    Multiplayer:
    - Create server
    - Find server
    - Connect to server
    - Control "Player"
    - Added some synchronization, so only your own "Player" will be
    controlled.
    
    TODO:
    Interpolation for network code
    Little prediction for smoother movement
    
    Non network related issues which should be done:
    Getting the car(s) act little more realistic.
    Also menus should be added for joining games choosing a map and a car.
    
    Add checkpoints and finish to be able to win the game.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -25,22 +25,23 @@ public class CarController : MonoBehaviour {
 
 	void FixedUpdate(){
 		float angle = Input.GetAxis ("Horizontal");
-		float speed = Input.GetAxis ("RightTrigger");
-		float breakSpeed = Input.GetAxis("LeftTrigger");
-		wheel_LB.motorTorque = maxTorque * speed;
-		wheel_RB.motorTorque = maxTorque * speed;
+		//float speed = Input.GetAxis ("RightTrigger");
+		//float breakSpeed = Input.GetAxis("LeftTrigger");
+		float speed = Input.GetAxis ("Vertical");
+		wheel_LB.motorTorque = maxTorque * -speed;
+		wheel_RB.motorTorque = maxTorque * -speed;
 		wheel_LF.steerAngle = maxSteeringAngle * angle;
 		wheel_RF.steerAngle = maxSteeringAngle * angle;
-		wheel_LB.brakeTorque = maxBreakTorque * breakSpeed;
-		wheel_RB.brakeTorque = maxBreakTorque * breakSpeed;
+		//wheel_LB.brakeTorque = maxBreakTorque * breakSpeed;
+		//wheel_RB.brakeTorque = maxBreakTorque * breakSpeed;
 	}
 
 
 	void Update(){
-		wheelModel_LF.Rotate (wheel_LF.rpm / 60 * 360 * Time.deltaTime, wheel_LF.steerAngle, 0);
+	/*	wheelModel_LF.Rotate (wheel_LF.rpm / 60 * 360 * Time.deltaTime, wheel_LF.steerAngle, 0);
 		wheelModel_RF.Rotate (0, wheel_RF.rpm / 60 * 360 * Time.deltaTime, wheel_RF.steerAngle);
 		wheelModel_RB.Rotate (0, wheel_RB.rpm / 60 * 360 * Time.deltaTime, 0);
-		wheelModel_LB.Rotate (0, wheel_LB.rpm / 60 * 360 * Time.deltaTime, 0);
+		wheelModel_LB.Rotate (0, wheel_LB.rpm / 60 * 360 * Time.deltaTime, 0);*/
 
 
 	}
