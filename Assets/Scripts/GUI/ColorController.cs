0e032e5662d5ad2ea250ebb7a53180182447601d
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-03-14 01:30:58 +0100

    GUIColor
    Created new scene for testing a GUI with sliders (canvas).
    To get the sliders to change the color of the background I added a
    ColorController class.
    This class takes the value from the sliders and creates a Color. This
    Color is displayed on the background.

---
+++ Assets/Scripts/GUI/ColorController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ColorController : MonoBehaviour {
+	public Text rText;
+	public Text gText;
+	public Text bText;
+
+	public Slider rSlider;
+	public Slider gSlider;
+	public Slider bSlider;
+
+	public Image panel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rText.text = "R: " + rSlider.value.ToString();
+		gText.text = "G: " + gSlider.value.ToString();
+		bText.text = "B: " + bSlider.value.ToString();
+
+		panel.material.color = new Color (rSlider.value, gSlider.value, bSlider.value, 1f);
+	}
+}
