6eeaf256ede56ca1792dab53f4211f44476d8491
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-03-21 18:32:41 +0100

    SimpleRigidBodyCar drivable
    It's drivable but steering doesn't work like it should. If you press the
    button to steer it keeps spinning.

--- Assets/CarControllerRigidBody.cs
+++ Assets/CarControllerRigidBody.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class CarControllerRigidBody : MonoBehaviour {
+	public Vector3 oldAngle;
+
+	public float maxSpeed = 100;
+	public float maxSteeringAngle = 1;
 
 	// Use this for initialization
 	void Start () {
-	
+		rigidbody.maxAngularVelocity = maxSpeed;
 	}
 	
 	// Update is called once per frame
@@ -16,9 +20,9 @@ public class CarControllerRigidBody : MonoBehaviour {
 	void FixedUpdate(){
 		float steering = Input.GetAxis ("Horizontal");
 		float speed = Input.GetAxis ("Vertical");
-		print (speed);
 		//rigidbody.AddForce (Vector3.left * 50000 * speed);
-		rigidbody.AddForce((Quaternion.Euler(transform.InverseTransformDirection(0,steering*90,0)) *Vector3.left) * 50000*speed);
-		//rigidbody.rotation = new  (0.0f, steering, 0.0f);
+		rigidbody.AddForce((Quaternion.Euler(transform.InverseTransformDirection(0,oldAngle.y + steering * maxSteeringAngle,0)) *Vector3.left) * 50000*speed);
+		transform.rotation = Quaternion.Euler (transform.InverseTransformDirection (0, oldAngle.y + steering * maxSteeringAngle, 0));
+		oldAngle = transform.eulerAngles;
 	}
 }
