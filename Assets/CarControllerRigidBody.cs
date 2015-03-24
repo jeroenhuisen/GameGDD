5955415fb9f957b56c583bea72bdfc6c05d1d1fe
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-03-24 18:10:51 +0100

    Some adjustments to the car (BACKUP4)
    tried alot of way to turn a car without making it glitch.
    It seems to work without collision in the wheels.
    
    This is also a backup I'm converting to Unity 5 after

--- Assets/CarControllerRigidBody.cs
+++ Assets/CarControllerRigidBody.cs
@@ -23,13 +23,21 @@ public class CarControllerRigidBody : MonoBehaviour {
 	void FixedUpdate(){
 		float steering = Input.GetAxis ("Horizontal");
 		float gas = Input.GetAxis ("Vertical");
-		//rigidbody.AddForce (Vector3.left * 50000 * speed);
-		rigidbody.AddForce((Quaternion.Euler(transform.InverseTransformDirection(0,oldAngle.y + steering * maxSteeringAngle,0)) *Vector3.back) * 50000*gas);
+		//rigidbody.AddForce((Quaternion.Euler(transform.InverseTransformDirection(0,oldAngle.y + steering * maxSteeringAngle,0)) *Vector3.back) * 50000*gas);
+/*		if (gas > 0) {
+			rigidbody.AddRelativeForce ((Quaternion.Euler (transform.InverseTransformDirection (0, steering * maxSteeringAngle, 0)) * Vector3.left) * 50000 * gas);
+		}
+		else if(gas < 0){
+			rigidbody.AddRelativeForce ((Quaternion.Euler (transform.InverseTransformDirection (0, steering * maxSteeringAngle, 0)) * Vector3.left) * 50000 * -gas);
+		}*/
+
+		rigidbody.AddRelativeForce (Vector3.back * gas * 100000, ForceMode.Force);
 //		Vector3 speed = rigidbody.velocity;
 		//if (speed.x > 0 || speed.y > 0 || speed.z > 0) {
 		//GetComponent<Rigidbody>().rotation = Quaternion.Euler (transform.InverseTransformDirection (0, oldAngle.y + steering * maxSteeringAngle, 0));
 		transform.rotation = Quaternion.Euler (transform.InverseTransformDirection (0, oldAngle.y + steering * maxSteeringAngle, 0));
-
+		//rigidbody.AddRelativeTorque (transform.up * steering * maxSteeringAngle * 1000);
+		//rigidbody.AddTorque (transform.up * gas);
 		//}
 		//leftWheel.rotation = Quaternion.Euler (transform.InverseTransformDirection (90, steering * maxSteeringAngle, 0));
 		//rightWheel.rotation = Quaternion.Euler (transform.InverseTransformDirection (90, steering * maxSteeringAngle, 0));
