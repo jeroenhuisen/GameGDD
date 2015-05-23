876a01123bd168afe35a4455743ee956067f96f6
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-05-23 11:55:30 +0200

    Some files I forgot to commit

---
+++ Assets/MaxPlayersSlider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class MaxPlayersSlider : MonoBehaviour {
+	public Slider Slider;
+	public NetworkManager networkM;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnhSliderChanged() {
+		networkM.setMaxPlayers((int)Slider.value);
+	}
+}
---
+++ Assets/QuitGame.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Quit(){
+#if UNITY_EDITOR
+		//set PlayMode to stop
+#else
+		Application.Quit ();
+#endif
+		Application.Quit ();
+		Debug.Log( "quit");
+	}
+}
