75d77abf4a0de84605b61547f14461dd89b49633
Jeroen Huisen <jeroen.huisen@student.hu.nl>
2015-06-02 08:49:48 +0200

    SelectMap forgot to commit it should be used for selecting the map for the server

---
+++ Assets/SelectMap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectMap : MonoBehaviour {
+    public GameManager gManager;
+    public Text mapNameButton;
+
+	// Use this for initialization
+	void Start () {
+        UpdateMapName();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void UpdateMapName(){
+        mapNameButton.text = gManager.getLevel();
+    }
+}
